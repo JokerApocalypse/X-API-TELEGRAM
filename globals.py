b6ea2fe8be5b803cfd0798c1ed0b7a777d2109da
X-TECH CORP <zabsonrejacques30@gmail.com>
2024-12-28 19:23:56 +0100

    globals.py

---
+++ globals.py
@@ -0,0 +1,10 @@
+# globals.py
+
+# Token du bot Telegram
+BOT_TOKEN = "TON_TELEGRAM_BOT_TOKEN"
+
+# Chemin vers le fichier JavaScript
+JS_FILE_PATH = "modules/xeontext17.js"
+
+# Limite d'attaques simultanées
+MAX_ACTIVE_ATTACKS = 5
